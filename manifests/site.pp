ef8a3e311ca70b9c4adf2b38c405833e35c6f3e7
Simon Rance <sirance@gmail.com>
2018-05-18 15:39:58 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,4 @@
 node default {
-  file { '/root/README':
-    ensure  => file,
-    content => 'Hello  world!',
-    owner   => 'root',
-  }
 }
 node 'master.puppter.vm' {
   include role::master_server
