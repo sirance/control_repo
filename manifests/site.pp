9501fb32fd3056aa849e85dbd3710fbf5bfcce86
Simon Rance <sirance@gmail.com>
2018-05-21 14:03:36 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 node /^web/ {
   include role::app_server
@@ -9,3 +13,4 @@ node /^web/ {
 node /^db/ {
   include role::db_server
 }
+
